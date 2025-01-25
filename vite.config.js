61f3148ffefb088a983147ca90bdc100f48df050
MUHAMMAD SAHRUL <m.sahrul59@gmail.com>
2025-01-25 22:43:41 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import path from "path";
 import { defineConfig } from "vite";
 
-export default {
+export default defineConfig({
   root: path.resolve(__dirname),
   server: {
     port: 8080,
     hot: true,
   },
-};
+});
