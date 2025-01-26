8ba7241994e2c58941c268f65c871ab497c5ab5b
MUHAMMAD SAHRUL <m.sahrul59@gmail.com>
2025-01-26 07:03:06 +0700

    first commit

--- supabase.js
+++ supabase.js
@@ -1,6 +1,17 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_DATABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.SUPABASE_DATABASE_URL;
+const supabaseServiceRoleKey = import.meta.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
+const supabaseJwtSecret = import.meta.env.SUPABASE_JWT_SECRET;
+const viteSupabaseUrl = import.meta.env.VITE_SUPABASE_DATABASE_URL;
+const viteSupabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseServiceRoleKey,
+  supabaseAnonKey,
+  supabaseJwtSecret,
+  viteSupabaseUrl,
+  viteSupabaseAnonKey
+);
