e39c81ff673f4c14158618c3d02c946c6e3ba404
MUHAMMAD SAHRUL <m.sahrul59@gmail.com>
2025-01-29 16:00:18 +0700

    update

--- src/assets/js/resetpassword.js
+++ src/assets/js/resetpassword.js
@@ -6,12 +6,12 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC5gN65t8IR-lLobeTCfyIAQXTDkW9vCmc",
+  authDomain: "authloginccg.firebaseapp.com",
+  projectId: "authloginccg",
+  storageBucket: "authloginccg.firebasestorage.app",
+  messagingSenderId: "649967513477",
+  appId: "1:649967513477:web:c813a2c15a19959eab1eec",
 };
 
 // Define base URL for redirects
