475aea258753a52611426c59cd2940a26fe0c0a1
MUHAMMAD SAHRUL <m.sahrul59@gmail.com>
2025-01-29 22:59:54 +0700

    update

--- src/assets/js/register.js
+++ src/assets/js/register.js
@@ -4,16 +4,15 @@ import {
   createUserWithEmailAndPassword,
   sendEmailVerification,
 } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
-require("dotenv").config();
 
-// Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyC5gN65t8IR-lLobeTCfyIAQXTDkW9vCmc",
+  authDomain: "authloginccg.firebaseapp.com",
+  projectId: "authloginccg",
+  storageBucket: "authloginccg.firebasestorage.app",
+  messagingSenderId: "649967513477",
+  appId: "1:649967513477:web:c813a2c15a19959eab1eec",
 };
 
 // Define base URL for redirects
